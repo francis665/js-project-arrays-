9e5f31a4ff799f4e2251822ea4ae9ed1056810f7
francis665 <80477382+francis665@users.noreply.github.com>
2021-06-10 15:35:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.pop());
+
+console.log(secretMessage.length);
+
+console.log(secretMessage.push('to', 'Program'));
+
+console.log(secretMessage[7] = 'right');
+
+console.log(secretMessage.shift());
+
+console.log(secretMessage.unshift('Programming'));
+
+console.log(secretMessage.splice(6, 5, 'know'));
+
+
+console.log(secretMessage.join());
